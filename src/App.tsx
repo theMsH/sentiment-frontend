1adb62214fc7eac8b6dbbd06b0d861ea4f27b202
Miika Haapanen <123482448+theMsH@users.noreply.github.com>
2025-02-06 10:48:01 +0200

    init dev and first layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Sentiment Analysis bot</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <form onSubmit={()=>{console.log()}}>
+          <input type="text" 
+            required
+            max="250" 
+            placeholder="Write something for me to analyse!"
+          ></input>
+        </form>
+        <div className="spacer-10"/>
+        <button type="submit">
+          Get sentiment
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
